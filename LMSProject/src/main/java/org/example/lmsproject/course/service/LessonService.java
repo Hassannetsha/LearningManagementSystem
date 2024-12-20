9718697df5fedf4f5495ca90f22672232a0b9ac8
maryamhossam <112872236+maryamm585@users.noreply.github.com>
2024-12-20 21:09:34 +0200

    Update LessonService.java

--- LMSProject/src/main/java/org/example/lmsproject/course/service/LessonService.java
+++ LMSProject/src/main/java/org/example/lmsproject/course/service/LessonService.java
@@ -1,13 +1,25 @@
 package org.example.lmsproject.course.service;
 
+import org.example.lmsproject.course.model.Course;
+import org.example.lmsproject.course.model.Lesson;
+import org.example.lmsproject.course.repository.CourseRepository;
+import org.example.lmsproject.course.repository.LessonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class LessonService {
+    @Autowired
+    private LessonRepository lessonRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
     private static final int OTP_LENGTH = 6;
     private Map<Long, String> lessonotp = new HashMap<>();
     private Map<Long, Long> otpexpiration = new HashMap<>();
@@ -38,5 +50,32 @@ public class LessonService {
 
         return true;
     }
+
+    public Lesson createLesson(Long courseId, Lesson lesson) {
+        Optional<Course> courseOpt = courseRepository.findById(courseId);
+        if (courseOpt.isEmpty()) {
+            throw new IllegalArgumentException("Course not found");
+        }
+        lesson.setCourse(courseOpt.get());
+        return lessonRepository.save(lesson);
+    }
+
+    public Lesson updateLesson(Long lessonId, Lesson updatedLesson) {
+        Lesson existingLesson = lessonRepository.findById(lessonId)
+                .orElseThrow(() -> new IllegalArgumentException("Lesson not found"));
+
+        existingLesson.setTitle(updatedLesson.getTitle());
+        existingLesson.setDescription(updatedLesson.getDescription());
+        existingLesson.setDateTime(updatedLesson.getDateTime());
+        return lessonRepository.save(existingLesson);
+    }
+
+    public void deleteLesson(Long lessonId) {
+        if (!lessonRepository.existsById(lessonId)) {
+            throw new IllegalArgumentException("Lesson not found");
+        }
+        lessonRepository.deleteById(lessonId);
+    }
+
 
 }
